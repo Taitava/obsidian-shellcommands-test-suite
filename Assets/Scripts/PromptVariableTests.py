46264367f757ee220c45cb8da4f54ffed327da5f
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2023-12-30 15:11:21 +0200

    PromptVariableTests.py: Fix folder name.

--- Assets/Scripts/PromptVariableTests.py
+++ Assets/Scripts/PromptVariableTests.py
@@ -9,14 +9,14 @@ arguments.pop(0) # The first argument is the script file's name, so discard it.
 
 # Helper variables.
 dateAndTime = datetime.datetime.now()
-sandboxFolderName = "Sandbox"
+folderName = "Performed tests"
 
 # Define correct values and received values.
 expectedValues = {
     "{{_mandatory_field}}": "example",
     "{{_field_with_default_value}}": "Default",
     "{{_optional_field}}": "", # Expected to be left empty.
-    "{{_field_using_variables}}": dateAndTime.strftime("%Y") + " " + sandboxFolderName,
+    "{{_field_using_variables}}": dateAndTime.strftime("%Y") + " " + folderName,
     "{{_multiline_field}}": """Line1
 Line2""",
     "{{_toggle_field}}": "ON",
